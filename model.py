68eb05cfee5494f2347efd2cf935b1504a2b17ad
LordSkloore <maloysantosso@gmail.com>
2019-05-13 19:59:50 -0400

    Add garbage collection

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from tensorflow.keras import Model, Sequential
 from tensorflow.keras.layers import Dense, Reshape, Input, Dropout, Conv2D, UpSampling2D, BatchNormalization, Activation, Flatten, LeakyReLU, ZeroPadding2D
 from tqdm import tqdm
 import os
+import gc
 import numpy as np
 import matplotlib.pyplot as plt
 class GenAdvNet:
@@ -143,6 +144,7 @@ class GenAdvNet:
         training_data = self.get_training_data(datafolders)
         print("Training Data Loaded")
         print("about to process training data")
+        gc.collect()
         training_data = training_data / 127.5 - 1.
         print("Training Data Processed")
         print("about to label images")
